db166479cb212236a6b14332240474d392f44d6d
lenowo_peter_wsl <pastukhovpp@gmail.com>
2021-04-24 14:13:44 +0100

    some understanding

--- misterBin.py
+++ misterBin.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import requests as r
+from urllib.parse import quote
+import base64
+
+def enc(s):
+	return quote(base64.b64encode(bytes(s, 'utf-8')))
 
 u = r'https://www.tmbc.gov.uk/do-it-online/miscellaneous-forms/refuse-and-recycling-collection-dates'
+u2 = r'https://www.tmbc.gov.uk/utils/tmbc_waste_services/handler.php?adr_uprn=MDEwMDEzOTIzMDk4&valid_address=MQ%3D%3D&csrf=ZWQxYzJlYjBjZWJiMjQxNTRkYzgwYjlmOTNkOWU4MTJkMDBmYzhkNA%3D%3D&sqss='
 
-pc = r'https://www.tmbc.gov.uk/utils/pclu.php?postcode='
+token = 'ed1c2eb0cebb24154dc80b9f93d9e812d00fc8d4'
+postcode = 'tn91fx'
+uprn = '010013923281'
 
-z = r'https://www.tmbc.gov.uk/utils/pclu.php?postcode=dG45IDJmYg%3D%3D&csrf=NzA5MWVhNDhhMDdkNDhkOGVlNDVmMGU5YTA5NmM0ZDIyZGQ5Yjk5Zg%3D%3D'
+req = f'https://www.tmbc.gov.uk/utils/pclu.php?postcode={enc(postcode)}&csrf={enc(token)}'
 
-z2 = r'https://www.tmbc.gov.uk/utils/pclu.php?postcode=dG45IDJmYg%3D%3D&csrf=NzA5MWVhNDhhMDdkNDhkOGVlNDVmMGU5YTA5NmM0ZDIyZGQ5Yjk5Zg%3D%3D'
+req2 = f'https://www.tmbc.gov.uk/utils/tmbc_waste_services/handler.php?adr_uprn={enc(uprn)}&valid_address=MQ%3D%3D&csrf={enc(token)}&sqss='
 
-# r = pd.read_json(z)
-# print(r)
-
-a = r.get(z)
+a = r.get(req)
 print(a.text)
