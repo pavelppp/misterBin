7cfe754a85ccdbaf1a99eba9444c0452029d2019
Pavel Pastukhov <pastukhovpp@gmail.com>
2021-04-27 14:13:56 +0100

    comments added

--- misterBin.py
+++ misterBin.py
@@ -1,35 +1,30 @@
-from html import parser
-import pandas as pd
-import requests
-from urllib.parse import quote
 import base64
+from urllib.parse import quote
+
+import requests
 from bs4 import BeautifulSoup as bs
-import urllib3 as ul
-
-u = r'https://www.tmbc.gov.uk/do-it-online/miscellaneous-forms/refuse-and-recycling-collection-dates'
-
-fp = 'refuse-and-recycling-collection-dates'
-page = open(fp)
-soup = bs(page.read(), features='lxml')
-#soup = bs(requests.get(u, allow_redirects=True).content)
-
-token = soup.find('input', attrs={'data-tmbc-token': 'csrf'}).attrs['value']
 
 def enc(s):
 	return quote(base64.b64encode(bytes(s, 'utf-8')))
 
+u = r'https://www.tmbc.gov.uk/do-it-online/miscellaneous-forms/refuse-and-recycling-collection-dates'
+
+# parse preloaded html
+soup = bs(open('refuse-and-recycling-collection-dates').read(), features='lxml')
+
+# OR load html (not working for some reason ATM)
+#soup = bs(requests.get(u, allow_redirects=True).content)
+
+token = soup.find('input', attrs={'data-tmbc-token': 'csrf'}).attrs['value']
 
 postcode = 'tn92fb'
 #uprn = '010013923281'
 uprn = '010013923098'
 
-req = f'https://www.tmbc.gov.uk/utils/pclu.php?postcode={enc(postcode)}&csrf={enc(token)}'
-req2 = f'https://www.tmbc.gov.uk/utils/tmbc_waste_services/handler.php?adr_uprn={enc(uprn)}&valid_address=MQ%3D%3D&csrf={enc(token)}&sqss='
+sget_addresses = f'https://www.tmbc.gov.uk/utils/pclu.php?postcode={enc(postcode)}&csrf={enc(token)}'
+sget_dates = f'https://www.tmbc.gov.uk/utils/tmbc_waste_services/handler.php?adr_uprn={enc(uprn)}&valid_address=MQ%3D%3D&csrf={enc(token)}&sqss='
+# with fresh token from line 17 use these ^^^ links in browser, line #28-30 will work but only with right headers added
 
-http_pool = ul.connection_from_url(req2)
-bz = http_pool.urlopen('GET', req)
-
-print(bz.data)
-
-a = requests.get(req2, allow_redirects=True, headers={"Sec-Fetch-Mode":"cors"})
-print(a.text)
+# not working, need to tweak headers to make it work
+addresses = requests.get(sget_addresses, allow_redirects=True, headers={"Sec-Fetch-Mode":"cors"})
+dates = requests.get(sget_dates, allow_redirects=True, headers={"Sec-Fetch-Mode":"cors"})
